4523e12100bd1795a15c4d99b3a5074db51e7c69
lmintsys <lubmin23@gmail.com>
2023-01-11 17:40:22 -0500

    added function to enter and validate password's length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
-// Assignment code here
+function getLength() {
+  let length;
+
+  while (true) {
+    length = parseInt(prompt("What would be the length of your password?"));
+
+    if (Number.isInteger(length) && length >= 8 && length <= 128) {
+      break;
+    }
+
+    alert("Please enter a valid number between 8 and 128");
+  }
+
+  return length;
+}
+
 
 
 // Get references to the #generate element
